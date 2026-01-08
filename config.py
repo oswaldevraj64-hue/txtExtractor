dff902552ae14c9c600e9a57bc6df9c041b74a47
oswaldevraj64-hue <oswaldevraj64@gmail.com>
2026-01-08 10:53:43 +0530

    Update BOT_TOKEN to a hardcoded value
    Replaced BOT_TOKEN environment variable with a hardcoded token.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8438866543:AAHJn5RPshkfrbyc9YoTPb37gxj3LduvugA")
     API_ID = int(os.environ.get("API_ID", "30560523"))
     API_HASH = os.environ.get("API_HASH", "17ab58948bc24d85d961a39e058478a2")
     AUTH_USERS = os.environ.get("AUTH_USERS", "6404676836")
