3f34a107b18cf2ed0619ef6cc457d2872f3cb3db
oswaldevraj64-hue <oswaldevraj64@gmail.com>
2026-02-01 08:54:18 +0530

    Update BOT_TOKEN and AUTH_USERS in config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8438866543:AAHJn5RPshkfrbyc9YoTPb37gxj3LduvugA")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8277242043:AAHWcp5qC3IXXb8mZMweeX4jMP5ktx83mEg")
     API_ID = int(os.environ.get("API_ID", "30560523"))
     API_HASH = os.environ.get("API_HASH", "17ab58948bc24d85d961a39e058478a2")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "6404676836")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "8342427239")
