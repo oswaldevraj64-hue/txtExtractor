6200b422fda45d5720946bbba8ee86dd3ba2b069
oswaldevraj64-hue <oswaldevraj64@gmail.com>
2026-02-01 12:38:49 +0530

    Set default AUTH_USERS in config.py

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = os.environ.get("AUTH_USERS")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "8342427239")
